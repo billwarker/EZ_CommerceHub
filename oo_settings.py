190d4db6b383da2b12948dfe2338c83d10828cfd
Will Barker <willbarker1995@gmail.com>
2018-01-22 12:22:45 -0500

    adjusted staples to new format

--- oo_settings.py
+++ oo_settings.py
@@ -2,4 +2,4 @@
 
 ### absolute path to star_interactive.db
 
-DATABASE = r"C:\Users\Will\Desktop\Exercises\Star Interactive\Database\star_interactive.db"
+DATABASE = r"C:\Users\Will\Desktop\Exercises\Star Interactive\SI_Database\star_interactive.db"
